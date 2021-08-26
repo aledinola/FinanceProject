b02a974120d5ade61b5d5e070f6723181bb67aea
Alessandro Di Nola <40523651+aledinola@users.noreply.github.com>
2021-08-26 18:00:52 +0200

    Add files via upload

--- makemoments.m
+++ makemoments.m
@@ -2,9 +2,19 @@ function [mom] = makemoments(par,sim)
 %UNTITLED4 Summary of this function goes here
 %   Detailed explanation goes here
 
+disp("Calculate moments...")
 disp("Not implemented yet")
 
 mom = struct();
+
+ave_prof  = mean(sim.prof(:));   % mean profitability
+ave_irate = mean(sim.Irate(:)); % mean investment
+std_irate = std(sim.Irate(:));  % volatility of investment
+
+mom.ave_prof  = ave_prof;
+mom.ave_irate = ave_irate;
+mom.std_irate = std_irate;
+
 
 end
 
